f56bf2c33b752c10e001aa3926b9905e68cde154
n-a-z-a <nadesper@gmail.com>
2022-05-25 12:41:40 +0100

    second commit

---
+++ src/main/java/com/nology/VendingMachine.java
@@ -0,0 +1,15 @@
+package com.nology;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingMachine {
+
+    private int maxAmount;
+    private int stock;
+
+    ArrayList<String> categories = new ArrayList<>(List.of("drink", "crisps", "chocolate bars", "cereal bars"));
+    
+
+
+}
